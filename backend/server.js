0a9718a5cac3888d4481759392be8bf5519a46aa
vishakha <vishakhatyagi0101@gmail.com>
2026-05-11 12:52:42 +0530

    fixed cors policy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@
 
 // // const app = express();
 
-// // app.use(cors());
+// // app.use(cors({
+origin: "https://team-task-manager-eight-delta.vercel.app",
+   credentials: true
+}));
 // // app.use(express.json());
 
 // // connectDB();
@@ -42,7 +45,10 @@
 // const app = express();
 
 // // Middleware
-// app.use(cors());
+// app.use(cors({
+origin: "https://team-task-manager-eight-delta.vercel.app",
+   credentials: true
+}));
 // app.use(express.json());
 
 // // DB Connection
@@ -80,7 +86,10 @@ const app = express();
 /* =========================
    MIDDLEWARE
 ========================= */
-app.use(cors());
+app.use(cors({
+   origin: "https://team-task-manager-eight-delta.vercel.app",
+   credentials: true
+}));
 app.use(express.json()); // ✅ REQUIRED for req.body
 
 /* =========================
@@ -103,7 +112,7 @@ app.use("/api/projects", projectRoutes);
    TEST ROUTE
 ========================= */
 app.get("/", (req, res) => {
-  res.json({ message: "API Running Successfully 🚀" });
+   res.json({ message: "API Running Successfully 🚀" });
 });
 
 /* =========================
@@ -112,5 +121,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+   console.log(`🔥 Server running on port ${PORT}`);
 });
